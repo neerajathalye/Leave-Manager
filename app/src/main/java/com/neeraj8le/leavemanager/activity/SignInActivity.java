ab801ab6c3b5120bc595a55d21754138294d45ca
Arnav Kaushal <Arnav Kaushal>
2017-03-31 20:24:19 +0530

    sign in and sign up activity

--- app/src/main/java/com/neeraj8le/leavemanager/activity/SignInActivity.java
+++ app/src/main/java/com/neeraj8le/leavemanager/activity/SignInActivity.java
@@ -1,25 +1,86 @@
 package com.neeraj8le.leavemanager.activity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.neeraj8le.leavemanager.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button signInButton;
+    private Button signUpButton;
+    EditText met1,met2;
+    TextView mtv1;
+    TextInputLayout til1,til2;
+    String email;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
+        til1=(TextInputLayout)findViewById(R.id.textInputLayout);
+        til2=(TextInputLayout)findViewById(R.id.textInputLayout);
+        met1=(EditText)findViewById(R.id.et1);
+        met2=(EditText)findViewById(R.id.et2);
+        mtv1=(TextView)findViewById(R.id.tv1);
+        signUpButton=(Button)findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(this);
         signInButton = (Button) findViewById(R.id.signInButton);
-
         signInButton.setOnClickListener(this);
+        email=til1.getEditText().getText().toString();
+        password=til2.getEditText().getText().toString();
+
+        mtv1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+                View alertLayout = inflater.inflate(R.layout.layout_custom_dialog,null);
+                final EditText etEmail = (EditText) alertLayout.findViewById(R.id.et_email);
+                final AlertDialog.Builder alert = new AlertDialog.Builder(SignInActivity.this);
+                alert.setTitle("Password Recovery");
+                // this is set the view from XML inside AlertDialog
+                alert.setView(alertLayout);
+                // disallow cancel of AlertDialog on click of back button and outside touch
+                alert.setCancelable(true);
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(alert.getContext(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+
+                        String email = etEmail.getText().toString();
+                    }
+                });
+                AlertDialog dialog = alert.create();
+                dialog.show();
+
+                return false;
+            }
+
+        });
     }
 
     @Override
@@ -30,6 +91,12 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
                 Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.signUpButton:
+                Intent intent1=new Intent(SignInActivity.this,SignUpActivity.class);
+                startActivity(intent1);
+                break;
         }
     }
+
+
 }
---
+++ app/src/main/java/com/neeraj8le/leavemanager/activity/SignUpActivity.java
@@ -0,0 +1,114 @@
+package com.neeraj8le.leavemanager.activity;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.neeraj8le.leavemanager.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SignUpActivity extends AppCompatActivity {
+    private Button mSubmit;
+    Spinner s1;
+    String supervisor[] = {"Arnav", "Neeraj", "Priyanshu", "Yolo"};
+
+    void showToast(String msg)
+
+    {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        s1 = (Spinner) findViewById(R.id.spinner1);
+        mSubmit = (Button) findViewById(R.id.submit);
+        final TextInputLayout til0 = (TextInputLayout) findViewById(R.id.textInputLayout0);
+        final TextInputLayout til1 = (TextInputLayout) findViewById(R.id.textInputLayout1);
+        final TextInputLayout til2 = (TextInputLayout) findViewById(R.id.textInputLayout2);
+        final TextInputLayout til3 = (TextInputLayout) findViewById(R.id.textInputLayout3);
+        final TextInputLayout til4 = (TextInputLayout) findViewById(R.id.textInputLayout4);
+        final TextInputLayout til6 = (TextInputLayout) findViewById(R.id.textInputLayout6);
+        final TextInputLayout til7 = (TextInputLayout) findViewById(R.id.textInputLayout7);
+        final TextInputLayout til8 = (TextInputLayout) findViewById(R.id.textInputLayout8);
+        final String emp_id = til0.getEditText().getText().toString();
+        final String name = til1.getEditText().getText().toString();
+        final String dept_name = til2.getEditText().getText().toString();
+        final String desig = til3.getEditText().getText().toString();
+        final String contact = til4.getEditText().getText().toString();
+        final String email = til6.getEditText().getText().toString();
+        final String password = til7.getEditText().getText().toString();
+        final String con_pass=til8.getEditText().getText().toString();
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, supervisor);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(adapter);
+        final String supervise = s1.getSelectedItem().toString();
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if(!(password.equals(con_pass)))
+                {
+                    til8.setError("Passwords do not match");
+                }
+                 if (name.equals("")) {
+                    til1.setError("Field cannot be empty");
+                } if (emp_id.equals("")) {
+                    til0.setError("Field cannot be empty");
+                } if (dept_name.equals("")) {
+                    til2.setError("Field cannot be empty");
+                } if (desig.equals("")) {
+                    til3.setError("Field cannot be empty");
+                } if (contact.equals("")) {
+                    til4.setError("Field cannot be empty");
+                } if (email.equals("")) {
+                    til6.setError("Field cannot be empty");
+                } if (password.equals("")) {
+                    til7.setError("Field cannot be empty");
+                }
+                if(con_pass.equals(""))
+                {
+                    til8.setError("Field cannot be empty");
+                }
+                if (contact.length() > 10) {
+                    til4.setError("Number cannot be more than 10 digits long");
+                } if (!isValidEmail(email)) {
+                    til6.setError("Invalid Email");
+                }
+            }
+
+        });
+
+        s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                showToast(supervisor[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                showToast("Not Selected");
+            }
+        });
+
+}
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+
+    }
+}
+
+
